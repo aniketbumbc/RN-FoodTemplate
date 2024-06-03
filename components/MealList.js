0f19b461659891d082c0e0e84242ffbcfa035bd5
aniket <aniket.umbc@gmail.com>
2024-06-03 22:24:05 +0530

    created new component

---
+++ components/MealList.js
@@ -0,0 +1,44 @@
+import { View, FlatList, StyleSheet } from 'react-native';
+import 
+
+function MealList({ items }) {
+  function renderMeal(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+
+    return (
+      <>
+        <MealItem {...mealItemProps} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.id}
+          renderItem={renderMeal}
+        />
+      </View>
+    </>
+  );
+}
+
+export default MealList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
